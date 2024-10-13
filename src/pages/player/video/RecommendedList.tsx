7ce6eb97cb7dfbb20d4811756b03f8950caf0225
hlaing-dev <hlaingmin1235145@gmail.com>
2024-10-13 14:17:20 +0700

    update

--- src/pages/player/video/RecommendedList.tsx
+++ src/pages/player/video/RecommendedList.tsx
@@ -5,11 +5,15 @@ const RecommendedList = ({ data }: any) => {
   return (
     <div className="pb-16 px-5">
       <h1 className="text-white mb-3 my-5">继续观看</h1>
-      <div className="flex justify-start items-center flex-wrap gap-4 mb-5">
+      <div 
+        className="grid gap-4 mb-5" 
+        style={{ 
+          gridTemplateColumns: 'repeat(3, 1fr)', 
+          gridGap: '16px' 
+        }}
+      >
         {data?.recommendList?.map((movie: any) => (
-          <div style={{width: '30%'}}>
-          <MovieCard movie={movie} height={"100px"} width={"200px"} />
-          </div>
+          <MovieCard key={movie.id} movie={movie} height={"100px"} width={"200px"} />
         ))}
       </div>
     </div>
