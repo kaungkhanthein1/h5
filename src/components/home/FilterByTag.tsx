f338b35f59d3847884a8c7a731f319741868ebd1
hlaing-dev <hlaingmin1235145@gmail.com>
2024-10-22 14:15:28 +0700

    update

--- src/components/home/FilterByTag.tsx
+++ src/components/home/FilterByTag.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setArea,
@@ -8,13 +8,50 @@ import {
 } from "../../pages/home/slice/HomeSlice";
 
 const FilterByTag = ({ data, sort }: any) => {
-  // console.log(data);
+  const selectedClassRef1 = useRef<any>(null);
+  const selectedYearRef1 = useRef<any>(null);
+  const selectedAreaRef1 = useRef<any>(null);
   const sortData = useSelector((state: any) => state.home.sort);
   const classData = useSelector((state: any) => state.home.class);
   const area = useSelector((state: any) => state.home.area);
   const year = useSelector((state: any) => state.home.year);
-
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (selectedYearRef1.current) {
+      setTimeout(() => {
+        selectedYearRef1.current?.scrollIntoView({
+          behavior: "smooth",
+          inline: "center", // Ensure horizontal centering
+          block: "nearest", // Keep vertical positioning intact
+        });
+      }, 1);
+    }
+  }, [year]);
+  useEffect(() => {
+    if (selectedClassRef1.current) {
+      setTimeout(() => {
+      selectedClassRef1.current?.scrollIntoView({
+        behavior: "smooth",
+        inline: "center", // Ensure horizontal centering
+        block: "nearest", // Keep vertical positioning intact
+      });
+    });
+    }
+  }, [classData]);
+
+  useEffect(() => {
+    if (selectedAreaRef1.current) {
+      setTimeout(() => {
+      selectedAreaRef1.current?.scrollIntoView({
+        behavior: "smooth",
+        inline: "center", // Ensure horizontal centering
+        block: "nearest", // Keep vertical positioning intact
+      });
+    });
+    }
+  }, [area]);
+
   return (
     <div className="w-full pt-5 pb-2 flex flex-col gap-3">
       <div className="flex overflow-x-scroll px-3 gap-5 remove-scrollbar items-center">
@@ -37,24 +74,34 @@ const FilterByTag = ({ data, sort }: any) => {
         ))}
       </div>
       <div className="flex overflow-x-scroll px-3 gap-5 remove-scrollbar items-center">
-        {data && data.length > 0 && data[0]?.class?.map((item: any, index: any) => (
-          <div className="relative" key={index}>
-            <p
-              onClick={() => dispatch(setClass(item))}
-              className={`${
-                classData === item
-                  ? "bg-gray-500/35 px-4 py-1 text-xs"
-                  : "text-[14px]"
-              } whitespace-nowrap py-1 rounded-full hover:text-white transition-colors`}
+        {data &&
+          data.length > 0 &&
+          data[0]?.class?.map((item: any, index: any) => (
+            <div
+              className="relative"
+              key={item}
+              ref={classData === item ? selectedClassRef1 : null}
             >
-              {item}
-            </p>
-          </div>
-        ))}
+              <p
+                onClick={() => dispatch(setClass(item))}
+                className={`${
+                  classData === item
+                    ? "bg-gray-500/35 px-4 py-1 text-xs"
+                    : "text-[14px]"
+                } whitespace-nowrap py-1 rounded-full hover:text-white transition-colors`}
+              >
+                {item}
+              </p>
+            </div>
+          ))}
       </div>
       <div className="flex overflow-x-scroll px-3 gap-5 remove-scrollbar items-center">
         {data[0]?.area?.map((item: any, index: any) => (
-          <div className="relative" key={index}>
+          <div
+            className="relative"
+            key={item}
+            ref={area === item ? selectedAreaRef1 : null}
+          >
             <p
               onClick={() => dispatch(setArea(item))}
               className={`${
@@ -70,7 +117,11 @@ const FilterByTag = ({ data, sort }: any) => {
       </div>
       <div className="flex overflow-x-scroll px-3 gap-5 remove-scrollbar items-center">
         {data[0]?.year?.map((item: any, index: any) => (
-          <div className="relative" key={index}>
+          <div
+            className="relative"
+            key={item}
+            ref={year === item ? selectedYearRef1 : null}
+          >
             <p
               onClick={() => dispatch(setYear(item))}
               className={`${
--- src/components/home/FilteredByType.tsx
+++ src/components/home/FilteredByType.tsx
@@ -115,6 +115,7 @@ const FilteredByType = () => {
                       </div>
                     }
                   >
+                    <></>
                     {/* {item} */}
                   </InfiniteScroll>
                 </>
