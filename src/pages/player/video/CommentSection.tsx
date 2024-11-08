994ebddb2d16d3ffa725f943913ade97d2581e4f
hlaing-dev <hlaingmin1235145@gmail.com>
2024-11-08 16:51:49 +0700

    update

--- src/pages/player/video/CommentSection.tsx
+++ src/pages/player/video/CommentSection.tsx
@@ -333,7 +333,7 @@ const CommentComponent: React.FC<CommentProps> = ({ movieId, lowerDivHeight }) =
       {/* </InfiniteScroll>  */}
       </>
       : 
-      <div className="flex justify-center items-center text-center comment-btn" style={{height: lowerDivHeight}}>
+      <div className="flex justify-center items-center text-center comment-btn" style={{height: lowerDivHeight, width: '100%'}}>
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
