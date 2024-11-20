607140e5e86685b30dbd0a8ac6d7662f666e7fe2
hlaing-dev <hlaingmin1235145@gmail.com>
2024-11-20 20:13:02 +0700

    update

--- src/pages/profile/components/settings/Versions.tsx
+++ src/pages/profile/components/settings/Versions.tsx
@@ -10,14 +10,14 @@ const Versions = () => {
             <h1 className="versions-text">V 3.0.1.6</h1>
           </div>
         </div>
-        <div className="p-first">
+        {/* <div className="p-first">
           <div className="flex gap-1 max-w-[230px] flex-col ">
             <h1 className="versions-title">清楚缓存</h1>
           </div>
           <div>
             <h1 className="versions-text">35.8 Mb</h1>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
