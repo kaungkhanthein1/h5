03244fdd1be043eac2e7136db522b9be18384c28
hlaing-dev <hlaingmin1235145@gmail.com>
2024-11-25 11:17:38 +0700

    update

--- src/pages/player/video/NetworkError.tsx
+++ src/pages/player/video/NetworkError.tsx
@@ -20,7 +20,18 @@ const NetworkError: React.FC<any> = ({ switchNow, refresh, onBack }) => {
           {/* Back Arrow */}
     <div className="absolute top-4 left-4 z-50">
       <button className="text-white text-2xl" onClick={onBack}>
-        ← {/* Replace with your actual back arrow component or icon */}
+        <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M7.828 11H20V13H7.828L13.192 18.364L11.778 19.778L4 12L11.778 4.22198L13.192 5.63598L7.828 11Z"
+                fill="white"
+              />
+            </svg>
       </button>
     </div>
 
