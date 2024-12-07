26b2a4362f4e03f0e503daf40a92d274dfaca2cf
hlaing-dev <hlaingmin1235145@gmail.com>
2024-12-07 10:25:42 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -100,6 +100,7 @@ const App: React.FC = () => {
       if (setGradiant) {
         element.style.background =
           "linear-gradient(119deg, #0D485B 0%, #1E2037 46.52%, #2D1232 77.4%)";
+        element.style.backgroundColor = "#161619";
       } else {
         element.style.background = "#161619";
       }
