006bd5d0a97d0273712be78e883a8f40d3763c75
hlaing-dev <hlaingmin1235145@gmail.com>
2024-12-14 12:21:03 +0700

    update

--- src/pages/player/video/SourceSelector.tsx
+++ src/pages/player/video/SourceSelector.tsx
@@ -64,7 +64,7 @@ const SourceSelector: React.FC<SourceSelectorProps> = ({
             <span className="font-bold">
               {movieDetail.play_from.filter(x => x.code === selectedEpisode?.from_code)[0]?.name || "未选择来源"}
             </span>{" "}
-            <span className="text-channelSecondary">{movieDetail.play_from?.length || 0} 个视频</span>
+            <span className="text-channelSecondary">{movieDetail?.play_from[selectedSource]?.total || 0} 个视频</span>
           </span>
         </div>
 
