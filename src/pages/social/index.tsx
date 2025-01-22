37f77ec5852f341f32c7c2a481389cff84fc418c
hlaing-dev <hlaingmin1235145@gmail.com>
2025-01-22 08:50:01 +0700

    update

--- src/pages/social/index.tsx
+++ src/pages/social/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import "./social.css";
+import { useDispatch } from "react-redux";
+import { setShowingDetail } from "../../features/login/ModelSlice";
 
 const Social = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(setShowingDetail(false))
+  },[])
   return (
     <div className="text-white">
       <Navbar />
